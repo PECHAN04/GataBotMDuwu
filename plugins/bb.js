fc2c4cec2f61966a81b8a736c3c4a21490772c43
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-05 20:49:26 -0500

    Update bb.js

--- plugins/bb.js
+++ plugins/bb.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './mediav.mp3'
+let vn = './media/bebe.mp3'
 conn.sendFile(m.chat, vn, 'bebe.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /abduzcan|Abduzcan|adbuzcan|Adbuzcan/ 
+handler.customPrefix = /bb|bebe|Bebe|Bb/ 
 handler.command = new RegExp
 module.exports = handler
