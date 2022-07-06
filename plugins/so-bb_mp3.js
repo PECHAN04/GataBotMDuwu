578b0067c20436b28678691802b92e8d0868a145
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-05 21:06:24 -0500

    Update so-bb_mp3.js

--- plugins/so-bb_mp3.js
+++ plugins/so-bb_mp3.js
@@ -6,12 +6,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
-conn.sendFile(m.chat, vn, 'abduzcan.mp3', null, m, true, {
+let vn = './media/bebe.mp3'
+conn.sendFile(m.chat, vn, 'bebe.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /abduzcan|Abduzcan|adbuzcan|Adbuzcan/ 
+handler.customPrefix = /Bb|bb|bebe|Bebe/ 
 handler.command = new RegExp
 export default handler
