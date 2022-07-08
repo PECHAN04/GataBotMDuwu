1d912f101cc28a8e922264161034068f09b15bad
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-07-07 19:15:12 -0500

    Update so-3delamañana_mp3.js

--- plugins/so-3delamañana_mp3.js
+++ plugins/so-3delamañana_mp3.js
@@ -6,12 +6,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/abduzcan.mp3'
-conn.sendFile(m.chat, vn, 'abduzcan.mp3', null, m, true, {
+let vn = './media/3mañana.mp3'
+conn.sendFile(m.chat, vn, '3mañana.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /abduzcan|Abduzcan|adbuzcan|Adbuzcan/ 
+handler.customPrefix = /D03|d03|Mañana|mañana/ 
 handler.command = new RegExp
 export default handler
